03b43d16645f495ea32063e4e9f438ae9793a4cf
daniel <skyrimdaniel11@gmail.com>
2025-02-11 17:37:10 -0300

    feat: install filament 3.2^

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //ORM -> Eloquent
+    User::create([
+        'name' => 'Lucas',
+        'email' => 'lucas@gmail.com',
+        'password' => bcrypt('password')
+    ]);
 });
